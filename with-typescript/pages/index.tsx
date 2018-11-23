3b8d3898ac8b25334a49bdfec3f9fbd94bfe5c25
hongymagic <hongymagic+github@me.com>
2018-11-23 11:14:18 +1100

    TypeScript and Styled Components have joined forced to form a new element!

--- with-typescript/pages/index.tsx
+++ with-typescript/pages/index.tsx
@@ -1,9 +1,11 @@
 import Link from 'next/link';
 import Layout from '../components/Layout';
+import Paragraph from '../components/Paragraph';
 
 export default () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
+    <Paragraph>Are we there yet? 🤔</Paragraph>
     <p><Link href='/about'><a>About</a></Link></p>
   </Layout>
 );
